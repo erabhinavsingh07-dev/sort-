7b6609ca54d00a35a4fa07d0e51a6ae1140bb393
erabhinavsingh07-dev <erabhinavsingh07@gmail.com>
2026-04-08 23:53:25 +0530

    Add files via upload

---
+++ inserting.py
@@ -0,0 +1,9 @@
+def insectinsort(arr)
+    for i in range( 1,n):
+      min = arr[i]
+      j= i-1
+      while j>=0 and arr[j]
+            arr[j+1]=arr[j]
+                   j-=1
+            arr [j+1]= min
+
